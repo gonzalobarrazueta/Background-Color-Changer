b87226038ca858ee32e7f2ac6ee5b70a91926d50
Gonzalo Barrazueta <gbryuzkns@gmail.com>
2020-09-06 18:07:47 -0500

    Implemented the gradient function

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ let gradient_btn = document.querySelector('.gradient');
 
 /* Cualquiera de los 2 es válido
 solid_btn.addEventListener('click', setSolidColor); */
-
 solid_btn.onclick = setSolidColor; 
 gradient_btn.onclick = setGradientColor;
 
@@ -15,9 +14,24 @@ function setSolidColor() {
   let green = Math.ceil(Math.random()*255);
   let blue = Math.ceil(Math.random()*255); 
       
-  background.style.backgroundColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+  background.style.background = 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
 
 function setGradientColor() {
+  
+  let red = Math.ceil(Math.random()*255);
+  let green = Math.ceil(Math.random()*255);
+  let blue = Math.ceil(Math.random()*255); 
+  
+  let red2 = Math.ceil(Math.random()*255);
+  let green2 = Math.ceil(Math.random()*255);
+  let blue2 = Math.ceil(Math.random()*255); 
 
+  let angle = Math.ceil(Math.random()*360);
+
+  background.style.background = 
+  
+  'linear-gradient(' + angle + 'deg,' +
+  'rgb(' + red + ',' + green + ',' + blue + '),' +
+  'rgb(' + red2 + ',' + green2 + ',' + blue2 + ')';
 }
